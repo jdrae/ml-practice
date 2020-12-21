8f80465f6fc5510f1a7108d813c2dbcbf1410478
jdrae <dr.kiwi.j@gmail.com>
2020-12-21 16:03:55 +0900

    day2

---
+++ deep learning from scratch/stochastic_gradient_descent.py
@@ -0,0 +1,104 @@
+import numpy as np
+
+from activation_function import sigmoid
+from output_layer import softmax
+from loss_function import cross_entropy_error
+from gradient_descent import numerical_gradient
+
+# Stanford CS231n
+class TwoLayerNet:
+    def __init__(self, input_size, hidden_size, output_size, weight_init_std=0.01):
+        # 가중치 초기화
+        self.params = {}
+        self.params['W1'] = weight_init_std * np.random.randn(input_size, hidden_size) # 정규분포
+        self.params['b1'] = np.zeros(hidden_size)
+        self.params['W2'] = weight_init_std * np.random.randn(hidden_size, output_size)
+        self.params['b2'] = np.zeros(output_size)
+
+
+    def predict(self, x):
+        W1, W2 = self.params['W1'], self.params['W2']
+        b1, b2 = self.params['b1'], self.params['b2']
+
+        a1 = np.dot(x, W1) + b1
+        z1 = sigmoid(a1)
+        a2 = np.dot(z1, W2) + b2
+        y = softmax(a2)
+
+        return y
+
+    def loss(self, x, t):
+        y = self.predict(x)
+
+        return cross_entropy_error(y, t)
+
+    def accuracy(self, x, t):
+        y = self.predict(x)
+        y = np.argmax(y, axis=1)
+        t = np.argmax(t, axis=1)
+
+        accuracy = np.sum(y==t) / float(x.shape[0])
+        return accuracy
+
+    def numerical_gradient(self, x, t):
+        loss_W = lambda W: self.loss(x,t)
+
+        grads = {}
+        grads['W1'] = numerical_gradient(loss_W, self.params['W1'])
+        grads['b1'] = numerical_gradient(loss_W, self.params['b1'])
+        grads['W2'] = numerical_gradient(loss_W, self.params['W2'])
+        grads['b2'] = numerical_gradient(loss_W, self.params['b2'])
+
+        return grads
+
+
+from dataset.mnist import load_mnist
+import matplotlib.pylab as plt
+
+if __name__ == "__main__":
+    (x_train, y_train), (x_test, y_test) = load_mnist(normalize= True, one_hot_label=True)
+
+    train_loss_list = []
+    train_acc_list = []
+    test_acc_list = []
+
+    # hyperparameter
+    iters_num = 10000
+    iter_per_epoch = max(train_size / batch_size, 1)
+    train_size = x_train.shape[0]
+    batch_size = 100
+    learning_rate = 0.1
+    network = TwoLayerNet(input_size = 784, hidden_size=50, output_size=10)
+
+    for i in range(iters_num):
+        print(i)
+        # acquire mini batch
+        batch_mask = np.random.choice(train_size, batch_size)
+        x_batch =  x_train[batch_mask]
+        y_batch = y_train[batch_mask]
+
+        # compute gradient
+        grad = network.numerical_gradient(x_batch, y_batch)
+        # grad = network.gradient(x_batch, y_batch) # backpropagation
+
+        # upgrade hyperparameters
+        for key in ('W1', 'b1', 'W2', 'b2'):
+            network.params[key] -= learning_rate * grad[key]
+
+        # log
+        loss = network.loss(x_batch, y_batch)
+        train_loss_list.append(loss)
+
+
+    if i % iter_per_epoch == 0:
+        train_acc = network.accuracy(x_train, y_train)
+        test_acc = network.accuracy(x_test,y_test)
+        train_acc_list.append(train_acc)
+        test_acc_list.append(test_acc)
+        print("train acc, test acc | ", train_acc, ",", test_acc)    
+
+    # 그래프 그리기
+    plt.plot(range(1,iters_num+1), train_loss_list, label='train acc')
+    plt.xlabel("iteration")
+    plt.ylabel("loss function")
+    plt.show()
